d34ce385b500391745d90e09ab3e40c166bca674
saudblaze <saudblaze@gmail.com>
2022-05-21 01:11:42 -0700

    = > srudent basic module completes

--- src/app/Pages/student-add-edit/student-add-edit.component.ts
+++ src/app/Pages/student-add-edit/student-add-edit.component.ts
@@ -1,4 +1,9 @@
+declare var $: any;
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppService } from 'src/app/Services/app.service';
+import { NotificationService } from 'src/app/Services/notification.service';
 
 @Component({
   selector: 'app-student-add-edit',
@@ -6,10 +11,176 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-add-edit.component.css']
 })
 export class StudentAddEditComponent implements OnInit {
+  StudentForm: FormGroup;
+  errorMessage;
+  StudentRegistrationForm = {
+    StudentId: 0,
+    StudentName: "",    
+    StudentFatherName: "",   
+    StudentSurName:"",
+    StudentPhone:"",
+    StudentWhatsapp:"",
+    BatchId:0,
+    FatherPhone:"",
+    FatherWhatsapp:"",
+    JoiningDate:"", 
+    StudentFileName:"",
+    StudentFolder:"",
+    ByteData: undefined,
+    StudentAddress1:"",
+    StudentAddress2:"",
+    StudentSchoolName:"",
+    StudentStandard:"",
+  };
+  submitted = false;
+  Leadid: any;
+  constructor(
+    public route: ActivatedRoute,
+    private formBuilder: FormBuilder, 
+    private appService: AppService, 
+    private router: Router, 
+    private notifyService: NotificationService
+    ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+      this.Leadid = this.route.snapshot.paramMap.get("id");
+      if (this.Leadid) {
+        this.GetStudentById(this.Leadid);
+      }
+      else{
+        setTimeout(() => { 
+          //this.isVisible = true 
+        }, 1000);  
+      }
+  
+      this.StudentForm = this.formBuilder.group(
+        {
+          StudentName: ["", [Validators.required]] ,
+          StudentFatherName: ["", [Validators.required]] ,
+          StudentSurName:["", [Validators.required]] ,
+          StudentPhone:["", [Validators.required]] ,
+          StudentWhatsapp:["", [Validators.required]] ,
+          BatchId:["", [Validators.required]] ,
+          FatherPhone:["", [Validators.required]] ,
+          FatherWhatsapp:["", [Validators.required]] ,
+          JoiningDate:["", [Validators.required]] ,
+          //StudentFileName:["", [Validators.required]] ,         
+          
+          StudentAddress1:["", [Validators.required]] ,
+          StudentAddress2:["", [Validators.required]] ,
+          StudentSchoolName:["", [Validators.required]] ,
+          StudentStandard:["", [Validators.required]] ,      
+        },
+        {
+          // Used custom form validator name
+          //validator: ComparePassword("Password", "ConfirmPassword")
+        }
+      );
+      
+    }
+    // Getter function in order to get form controls value
+    get f() {
+      return this.StudentForm.controls;
+    }
+  
+    BackToList() {
+      this.router.navigate(['Student']);
+    }
+  
+    
+    ClearForm() {
+      this.StudentRegistrationForm.StudentName = "";    
+      this.StudentRegistrationForm.StudentFatherName = "";        
+    }
+  
+    GetStudentById(Id){
+      this.appService.GetWithToken("Student", "GetStudentById?Id="+Id)
+  
+        .subscribe(
+          (res: any) => {          
+            var objResult = res//.json()
+            if (objResult && objResult.ErrorNumber == "0") {
+              this.StudentRegistrationForm.StudentId = objResult.Data.StudentId;
+              this.StudentRegistrationForm.StudentName = objResult.Data.StudentName; 
+              this.StudentRegistrationForm.StudentFatherName = objResult.Data.StudentFatherName;
+              this.StudentRegistrationForm.StudentSurName = objResult.Data.StudentSurName; 
+              this.StudentRegistrationForm.StudentPhone = objResult.Data.StudentPhone;     
+              this.StudentRegistrationForm.StudentWhatsapp = objResult.Data.StudentWhatsapp;     
+              this.StudentRegistrationForm.BatchId = objResult.Data.BatchId;     
+              this.StudentRegistrationForm.FatherPhone = objResult.Data.FatherPhone;     
+              this.StudentRegistrationForm.FatherWhatsapp = objResult.Data.FatherWhatsapp;     
+              this.StudentRegistrationForm.JoiningDate = objResult.Data.JoiningDate;     
+              this.StudentRegistrationForm.StudentFileName = objResult.Data.StudentFileName;     
+              this.StudentRegistrationForm.StudentAddress1 = objResult.Data.StudentAddress1;     
+              this.StudentRegistrationForm.StudentAddress2 = objResult.Data.StudentAddress2;     
+              this.StudentRegistrationForm.StudentSchoolName = objResult.Data.StudentSchoolName;     
+              this.StudentRegistrationForm.StudentStandard = objResult.Data.StudentStandard;     
+            }
+          },
+          error =>
+            this.errorMessage = <any>error
+  
+        );
+    }  
+  
+    SubmitHotel() {
+      debugger
+      this.submitted = true;
+      // Returns false if form is invalid
+      if (this.StudentForm.invalid) {
+        return;
+      }
+      var objHotel = {
+        "StudentId": this.StudentRegistrationForm.StudentId,
+        "StudentName": this.StudentRegistrationForm.StudentName,
+        "StudentFatherName": this.StudentRegistrationForm.StudentFatherName,
+        "StudentSurName": this.StudentRegistrationForm.StudentSurName,
+        "StudentPhone": this.StudentRegistrationForm.StudentPhone,
+        "StudentWhatsapp": this.StudentRegistrationForm.StudentWhatsapp,
+        "BatchId": this.StudentRegistrationForm.BatchId,
+        "FatherPhone": this.StudentRegistrationForm.FatherPhone,
+        "FatherWhatsapp": this.StudentRegistrationForm.FatherWhatsapp,
+        "JoiningDate": this.StudentRegistrationForm.JoiningDate,
+        "StudentFileName": this.StudentRegistrationForm.StudentFileName,
+        "StudentFolder": this.StudentRegistrationForm.StudentFolder,
+        "StudentAddress1": this.StudentRegistrationForm.StudentAddress1,
+        "StudentAddress2": this.StudentRegistrationForm.StudentAddress2,
+        "StudentSchoolName": this.StudentRegistrationForm.StudentSchoolName,
+        "StudentStandard": this.StudentRegistrationForm.StudentStandard,
+        "CreatedBy":localStorage.getItem("UserId")  ,
+        "UpdatedBy":localStorage.getItem("UserId")  
+      };
+  
+      this.appService.PostToken("Student", "SaveStudent", objHotel)
+        .subscribe(
+          (res: any) => {
+            if (res) {
+              var resultbody = res//.json()
+              if (resultbody.ErrorNumber == 0) {
+                this.notifyService.showSuccess("Student successfully added.", "")
+                this.router.navigate(['Student']);
+              }
+              else if (resultbody.ErrorNumber == 1) {
+                this.notifyService.showSuccess("Student successfully updated.", "")
+                this.router.navigate(['Student']);
+              }
+              else
+              {
+                this.notifyService.showError(resultbody.ErrorMessage, "Error");  
+              }
+              
+            } else {
+              this.notifyService.showError(this.errorMessage.statusText, "An error occured while registering Student.")
+            }
+  
+          },
+          error => {
+            this.errorMessage = <any>error
+            //var jsonObject = JSON.stringify(this.errorMessage);
+            this.notifyService.showError(this.errorMessage.statusText, "An error occured while registering Subject.")
+          });
+  
+  
+    }
 
 }
--- src/app/Pages/student/student.component.ts
+++ src/app/Pages/student/student.component.ts
@@ -1,15 +1,220 @@
-import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AppService } from 'src/app/Services/app.service';
+import { NotificationService } from 'src/app/Services/notification.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
-})
-export class StudentComponent implements OnInit {
+})          
+export class StudentComponent  implements OnInit {
+  @ViewChild('mydataTable', { static: false }) primarySampleComponent: ElementRef;
+  dataTable: any;
+  dtOption: any = {};
 
-  constructor() { }
+  StudentToBeDeleted = {
+    StudentId: 0,
+    StudentName: "",    
+    StudentFatherName: "",   
+    StudentSurName:"",
+    StudentPhone:"",
+    StudentWhatsapp:"",
+    BatchId:0,
+    FatherPhone:"",
+    FatherWhatsapp:"",
+    JoiningDate:"", 
+    StudentFileName:"",
+    StudentFolder:"",
+    ByteData: undefined,
+    StudentAddress1:"",
+    StudentAddress2:"",
+    StudentSchoolName:"",
+    StudentStandard:"",
+  };
+  
+  StateSearchForm = {
+    SubjectName: ""    ,
+    Abbreviations:""
+  }
+  errorMessage;
+  
+  submitted = false;
+  dDatatable: any;
+  SystemSettings;
+  constructor(
+    private router: Router, 
+    private notifyService: NotificationService, 
+    private formBuilder: FormBuilder, 
+    private appService: AppService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.SystemSettings = JSON.parse(localStorage.getItem("systemsettings"));
+  }
+
+  GetStudentById(Id) {
+    this.router.navigate(['StudentAddEdit', Id]);
+  }
+
+  SearchHotel() {
+    this.dDatatable.draw();
+  }
+  ClearSearch() {
+    this.StateSearchForm = {
+      SubjectName: ""      ,
+      Abbreviations: ""      
+    }
+    this.dDatatable.draw();
+  }
+
+  DeleteStateConfirmation(data) {
+    this.StudentToBeDeleted = data
+    $('#myModal').modal('show');
+  }
+  CloseModal() {
+    $('#myModal').modal('hide');
+  }
+  DeleteStudent() {
+    this.appService.GetWithToken("Student", "DeleteStudentById?Id=" + this.StudentToBeDeleted.StudentId + "&UpdatedBy=" + localStorage.getItem("UserId"))
+
+      .subscribe(
+        (res: any) => {
+          var objResult = res//.json()
+          if (objResult && objResult.ErrorNumber == "0") {
+            this.StudentToBeDeleted = {
+              StudentId: 0,
+              StudentName: "",    
+              StudentFatherName: "",   
+              StudentSurName:"",
+              StudentPhone:"",
+              StudentWhatsapp:"",
+              BatchId:0,
+              FatherPhone:"",
+              FatherWhatsapp:"",
+              JoiningDate:"", 
+              StudentFileName:"",
+              StudentFolder:"",
+              ByteData: undefined,
+              StudentAddress1:"",
+              StudentAddress2:"",
+              StudentSchoolName:"",
+              StudentStandard:"",  
+            };
+            $('#myModal').modal('hide');
+            this.notifyService.showError("Student successfully delete.", "")
+            this.dDatatable.draw();
+          }
+        },
+        error =>
+          this.errorMessage = <any>error
+
+      );
+  }
+
+  ngAfterViewInit() {
+    this.GetStateList();
+  }
+
+  GetStateList() {
+
+    let me = this;
+    this.dtOption = {
+      "searching": false,
+      "serverSide": true,
+      "ordering": false,
+      "drawCallback": this.InitStaffControl(),
+      // "fnDrawCallback": function( oSettings ) {
+      //   alert( 'DataTables has redrawn the table' );
+      // },
+      "createdRow": function (row, data, dataIndex) {
+        $(".btnEdit", $(row)).click(() => {
+          me.GetStudentById(data.StudentId);
+        });
+        $(".btnDelete", $(row)).click(() => {
+          me.DeleteStateConfirmation(data);
+        });
+
+      },
+      "dom": '<"pull-right"B><"clear">ltip',
+      "buttons": [
+        {
+
+          "extend": 'excelHtml5',
+          "title": 'Staffs',
+          "exportOptions": {
+            columns: "thead th:not(.noExport)"
+          }
+        },
+        {
+          "extend": 'csvHtml5',
+          "title": 'Staffs',
+          "exportOptions": {
+            columns: "thead th:not(.noExport)"
+          }
+        }
+      ],
+      "ajax": {
+        "url": environment.apiUrl + "api/Student/GetStudentList",
+        "type": "POST",
+        "data": function (data) {
+          // data.StateName = me.StateSearchForm.StateName;
+          // data.Abbreviations = me.StateSearchForm.Abbreviations;     
+
+           data.SubjectName = me.StateSearchForm.SubjectName;
+           data.Abbreviations = me.StateSearchForm.Abbreviations;             
+        }
+      },
+      "columns": [
+        {
+          "title": "ID",
+          "data": "StudentId",
+          "class": "colwidth50"     
+        },
+        {
+          "title": "Name",
+          "data": "StudentName",
+          "class": "colwidth300"      
+        },
+        {
+          "title": "Father Name",
+          "data": "StudentFatherName",
+          "class": "colwidth300"      
+        },
+        {
+          "title": "Action",
+          "data": "StudentId",
+          "render": function (data) {
+            var ret = '';
+            if (me.SystemSettings.objStudent.Update)
+            {
+              ret = "<button type='button' Id='" + data + "' class='btn btn-success btn-xs btnEdit'><i class='fa fa-edit'></i> Edit</button> ";
+            }            
+            if (me.SystemSettings.objStudent.Delete)
+            {
+              ret += "<button type='button' Id='" + data + "' class='btn btn-danger btn-xs btnDelete'><i class='fas fa-trash'></i> Delete</button>";
+            } 
+            return ret;
+          },
+          "class": "noExport"
+        }
+      ]
+    };
+    this.dataTable = $(this.primarySampleComponent.nativeElement);
+    this.dDatatable = this.dataTable.DataTable(this.dtOption);
+  }
+
+  InitStaffControl() {
+    // $(".btnEdit").click(() => {      
+    // });
+  }
+
+  AddNew() {
+    this.router.navigate(['StudentAddEdit']);
   }
 
 }
