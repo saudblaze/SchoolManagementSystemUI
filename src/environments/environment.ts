e7046c332e8fd0df186eb7a200443420c703a560
shimyon <shimyon@hotmail.com>
2023-01-19 11:16:26 +0530

    local check

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
 
   //local
   //apiUrl: 'http://192.168.0.5:8051/'
-  apiUrl: 'http://localhost:8051/'
+  // apiUrl: 'http://localhost:8051/'
+  apiUrl: 'http://mastereducation.in/api/'
   // //Live
   // apiUrl: 'https://webapi.quinify.com/'
 };
